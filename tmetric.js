d3a10aea185b862f9478b6241e7c8db3b0cd53cf
Kris Vandebroek <krisvandebroek@users.noreply.github.com>
2023-02-09 09:15:22 +0100

    Update tmetric.js

--- tmetric.js
+++ tmetric.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         TMetric Shortcuts
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Easy Shortcuts for TMetric
 // @author       Kris Vandebroek
 // @match        https://app.tmetric.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tmetric.com
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/krisvandebroek/tampermonkey-scripts/main/tmetric.js
+// @downloadURL  https://raw.githubusercontent.com/krisvandebroek/tampermonkey-scripts/main/tmetric.js
 // ==/UserScript==
 
 /*
